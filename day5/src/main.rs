6e30a33216405e5dd4133347c31a510f805232a0
KyleGBC <kyleroleson@gmail.com>
2023-12-06 16:33:04 -0500

    Optimizing day 5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,5 +1,10 @@
+use rayon::prelude::*;
+
+struct SeedRange { start: u64, end: u64 }
+
+
 #[derive(Debug, Clone)]
-struct Map { dest: u32, src: u32, len: u32 }
+struct Map { dest: u64, src: u64, len: u64 }
 impl Map
 {
     fn parse(s: &str) -> Map
@@ -10,31 +15,61 @@ impl Map
        let len = iter.next().unwrap().parse().unwrap();
        Map { dest, src, len}
     }
-    fn map_seed(&self, seed: u32) -> Option<u32>
-    {
+    fn map_seed(&self, seed: u64) -> Option<u64>
+    { 
         let diff = seed.checked_sub(self.src)?;
         return if diff > self.len { None } else { Some(self.dest + diff) }
     }
+    fn contains(self, seed: u64) -> bool { return self.src <= seed && seed <= self.src + self.len }
 }
 type Layer = Vec<Map>;
 
-fn map_seed_layer(layer: &Layer, seed: u32) -> u32
+fn map_seed_layer(layer: &Layer, seed: u64) -> u64
 {
     for map in layer { if let Some(mapped) = map.map_seed(seed) { return mapped } }
     return seed
 }
 
-fn map_seed_almanac(mut seed: u32, layers: &Vec<Layer>) -> u32
+fn map_seed_almanac(mut seed: u64, layers: &Vec<Layer>) -> u64
 {
     layers.iter().for_each(|l| seed = map_seed_layer(l, seed) );
     seed
 }
 
+fn process_range_layer(mut range: SeedRange, layer: &Layer) -> Vec<SeedRange>
+{
+    let mut ranges = Vec::new();
+    let i = 0;
+    while i < ranges.len()
+    {
+        let current_range = ranges[i];
+
+        for map in layer
+        {
+           // Five cases:
+           // Case 1: Wholly contained withing mapping range
+           // Case 2 Left contained, right not contained
+           // Case 3: Right contained, left not contained
+           // Case 4: Mapping contained wholly withing seed range, necessitates three-way split
+           // Case 5: No overlap
+        }
+    }
+    ranges
+}
+
+fn process_range(mut range: SeedRange, layers: &Vec<Layer>) -> Vec<SeedRange>
+{
+    let mut ranges = Vec::new();
+    for layer in layers { ranges = process_range_layer(range, layer); }
+    ranges
+}
+
 fn main() {
+    let time = std::time::Instant::now();
     let input = include_str!("../input.txt");
     let mut lines = input.lines();
     let (_, seeds) = lines.next().unwrap().split_once(':').unwrap();
-    let seeds: Vec<u32> = seeds.split_whitespace().map(|n| n.parse().unwrap()).collect();
+    let seeds: Vec<u64> = seeds.split_whitespace().map(|n| n.parse().unwrap()).collect();
 
     lines.next();
 
@@ -58,6 +93,11 @@ fn main() {
     layers.push(layer);
     
     let part1 = seeds.iter().map(|s| map_seed_almanac(*s, &layers)).min().unwrap();
-    println!("{part1}");
+
+    let mut seed_ranges: Vec<SeedRange> = seeds.chunks(2).map(|v| SeedRange { start: v[0], end: v[1] }).map(|s| process_range(s, &layers)).flatten().collect();
+
+
+    
+    println!("{part1}, {part2} in {:?}", time.elapsed());
 
 }
